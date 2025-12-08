dc9ad03eccb96de0a3267978920ce5a64635228f
nietFreek <128623776+nietFreek@users.noreply.github.com>
2025-12-08 14:58:04 +0100

    Plan for SSL striping

---
+++ MITM.py
@@ -0,0 +1,8 @@
+# Spoof gateway
+# Listen for request from client
+# do either ARP if server on local network, otherwise gateway is already spoofed
+# Complete tcp handshake
+# Wait for http
+# If ssl stripping, start http connection using ssl stripping
+# If not, just start https connection
+
